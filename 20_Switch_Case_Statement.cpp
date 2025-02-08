78270414ce9901d1f249a54612c1d43271cd8efe
Lokesh Kumar <lokeshkumar797877@gmail.com>
2025-02-08 13:39:27 +0530

    DSA Problem

---
+++ 20_Switch_Case_Statement.cpp
@@ -0,0 +1,18 @@
+// class Solution {
+//     public:
+//       double switchCase(int choice, vector<double> &arr) {
+//               int R = arr[0];
+//               int L = arr[0];
+//               int B = arr[1];
+//               double ans = -1;
+//               double pi = 2*acos(0.0);
+//               switch (choice) {
+//                   case 1:
+//                   ans = (pi)*R*R ;
+//                   return ans;
+//                   case 2:
+//                   ans = L*B ;
+//                   return ans;
+//               }
+//       }
+//   };
