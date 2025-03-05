faa70dd843ec013a2140ec41bc6e05c25cab08a2
Lokesh Kumar <lokeshkumar797877@gmail.com>
2025-03-06 01:10:41 +0530

    DSA Problem

---
+++ 60_print_array.cpp
@@ -0,0 +1,10 @@
+// class Solution {
+//     public:
+//       // Just print the space seperated array elements
+//       void printArray(vector<int> &arr) {
+//           int n = arr.size();
+//           for(int i=0; i<n; i++){
+//               cout<<arr[i]<<" ";
+//           }
+//       }
+//   };
