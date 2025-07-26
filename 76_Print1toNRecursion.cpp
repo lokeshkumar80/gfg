76fcab245bdcb0b71b6a3e44be175768a49e41c5
Lokesh Kumar <lokeshkumar797877@gmail.com>
2025-07-26 11:52:23 +0530

    DSA Problem

---
+++ 76_Print1toNRecursion.cpp
@@ -0,0 +1,11 @@
+// class Solution {
+//   public:
+//     void printTillN(int n) {
+//         if(n==1){
+//             cout<<n<<" ";
+//             return;
+//         }
+//         printTillN(n-1);
+//         cout<<n<<" ";
+//     }
+// };
